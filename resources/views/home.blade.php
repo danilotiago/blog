e0eeae29da2d10db8144ae81230aaa099d341154
Danilo Tiago <danilo.tiago@sppost.com.br>
2018-01-09 17:41:11 -0200

    componente painel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <example-component></example-component>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <painel titulo="Meu painel">
+
+                    <div class="col-md-4">
+                        <painel titulo="Conteúdo 1"></painel>
+                    </div>
+
+                    <div class="col-md-4">
+                        <painel titulo="Conteúdo 2"></painel>
+                    </div>
+
+                    <div class="col-md-4">
+                        <painel titulo="Conteúdo 3"></painel>
+                    </div>
+
+                </painel>
+            </div>
+        </div>
+    </div>
 @endsection
