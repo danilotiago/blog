7c69e576e90a8c7e2c38d35570d91be862cb5f37
Danilo Tiago <danilo.tiago@sppost.com.br>
2018-01-10 16:59:06 -0200

    criando tabela lista de artigos

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -1,10 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <pagina tamanho="8">
+    <pagina tamanho="12">
         <painel titulo="Lista de artigos" cor="panel-primary">
 
+        <a href="#">Criar</a>
 
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Título</th>
+                        <th>Descrição</th>
+                        <th>Autor</th>
+                        <th>Data publicação</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Título de teste</td>
+                        <td>Descrição de teste</td>
+                        <td>Autor de teste</td>
+                        <td>99/99/9999</td>
+                        <td>
+                            <a href="#">Editar</a> |
+                            <a href="#">Deletar</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
         </painel>
     </pagina>
